204fe9692e74e5ea1702ccf50f863d0e12f9bc73
roberta.c04@aluno.ifsc.edu.br <roberta.c04@aluno.ifsc.edu.br>
2022-08-18 09:50:19 -0300

    projeto

---
+++ projeto_integrador/src/controle/LivroBD.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class LivroBD {
+	
+	// metodo de inserir livro
+	
+	// metodo de remover livro
+	
+	// metodo de atualizar livro
+	
+	// metodo de consulta um livro (por id)
+	
+	// metodo de consulta de todos os livros / listar todos os livro
+
+}
