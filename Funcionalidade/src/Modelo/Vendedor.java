5cf8e5faf731de5271d338249d88e6f7ede0d4ae
Sabrina <sabrina.c26@aluno.ifsc.edu.com>
2022-09-21 19:54:48 -0300

    Código inicial
    Tela de Login Pronta
    Pacote Modelo pronto

---
+++ Funcionalidade/src/Modelo/Vendedor.java
@@ -0,0 +1,61 @@
+package Modelo;
+
+public class Vendedor {
+	
+	private int idVendedor;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String cep;
+	private String endereco;
+	private float comissao;
+	
+	
+	
+	public int getIdVendedor() {
+		return idVendedor;
+	}
+	public void setIdVendedor(int idVendedor) {
+		this.idVendedor = idVendedor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public float getComissao() {
+		return comissao;
+	}
+	public void setComissao(float comissao) {
+		this.comissao = comissao;
+	}
+	
+	
+	
+	
+}
---
+++ Funcionalidade/src/visao/Main.java
@@ -0,0 +1,33 @@
+package visao;
+	
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		
+		try {
+			Parent rootContainer = FXMLLoader.load(getClass().getResource("TelaLogin.fxml"));
+            Scene scene = new Scene(rootContainer);
+            primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+}
