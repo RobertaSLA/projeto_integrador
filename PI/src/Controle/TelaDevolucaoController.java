7d1441121883ebe6c86d4dec0a0cfc2bd8bf1622
Sabrina <sabrina.c26@aluno.ifsc.edu>
2022-10-19 08:15:54 -0300

    Código Fonte

--- PI/src/Controle/TelaDevolucaoController.java
+++ PI/src/Controle/TelaDevolucaoController.java
@@ -11,7 +11,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class TelaVendaController {
+public class TelaDevolucaoController {
 
     @FXML
     private Button btnAcessoCadastroCliente;
