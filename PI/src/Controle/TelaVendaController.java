cdfac9f4915ca140ef8ed2eeaa016b96827b71b0
roberta <roberta.c04@aluno.ifsc.edu.br>
2022-11-23 10:43:36 -0300

    Telas - Cores

--- PI/src/Controle/TelaVendaController.java
+++ PI/src/Controle/TelaVendaController.java
@@ -223,8 +223,9 @@ public class TelaVendaController {
     }
 
     @FXML
-    void AcessoProcessamentoVenda(ActionEvent event) {
-
+    void AcessoProcessamentoVenda(ActionEvent event) throws IOException {
+    	AnchorPane telaHomePane = FXMLLoader.load(getClass().getResource("/visao/TelaVenda.fxml"));
+    	layoutListagem.getChildren().setAll(telaHomePane);
     }
     
     @FXML
